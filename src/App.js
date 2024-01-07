21e9e54fecf22aad269c59c41773f919a56439a5
Amit Kumar <15000amitkumar@gmail.com>
2024-01-07 07:20:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Blogs from "./components/Blogs";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Blogs />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
